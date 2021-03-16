3883199a5199acab2a1f1c22c89cca8630e02ed6
mareerste <80028910+mareerste@users.noreply.github.com>
2021-03-16 22:08:48 +0100

    Store: First bug

--- StoreProject/src/Store.java
+++ StoreProject/src/Store.java
@@ -1,8 +1,50 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
 
 public class Store {
 
 	public static void main(String[] args) {
-		// Lets do this
+		
+		
+		ArrayList<HashMap<String, String>> inventory = new ArrayList<>();
+		
+		HashMap<String, String> product = new HashMap<>();
+		product.put("name", "Tastatyura XZ");
+		product.put("price", "3999");
+		product.put("quantity", "3");
+		inventory.add(product);
+		
+		product = new HashMap<>();
+		product.put("name", "Tastatyura XY");
+		product.put("price", "3000");
+		product.put("quantity", "5");
+		inventory.add(product);
+		
+		
+//		inventory.add("Tastatyura XZ");
+//		inventory.add("Tastatyura YY");
+//		inventory.add("Televizor LG KY");
+//		inventory.add("Televizor Samsung X");
+//		inventory.add("Ves masina 12");
+//		inventory.add("Ves masina YY");
+//		inventory.add("Ves masina X4");
+//		inventory.add("Pegla TT");
+//		inventory.add("Pegla TX");
+		
+		
+		
+		System.out.println("Unesite parametar pretrage:");
+		Scanner scanner = new Scanner(System.in);
+		String searchText = scanner.nextLine();
+		
+		for (HashMap<String, String> p : inventory) {
+			if (p.toLowerCase().contains(searchText.toLowerCase())) {
+				System.out.println(p);
+			}
+		}
+		
 
 	}
 
