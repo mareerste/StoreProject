84de1b2e13d36a5c5cd31b8e156b6eb9a21b7b0c
mareerste <80028910+mareerste@users.noreply.github.com>
2021-03-18 00:00:51 +0100

    feat: start menu

--- StoreProject/src/Store.java
+++ StoreProject/src/Store.java
@@ -9,32 +9,52 @@ public class Store {
 		
 		ArrayList<Product> inventory = createInventory();
 		
-//		Ispis totalne vrednosti inventorya
+		pocetniMeni();
 		
-		int sum = 0;
-		for (Product p : inventory) {
-			double price = p.price;
-			double quantity = p.quantity;
-			sum += price * quantity;
+		System.out.println("Enter your value: ");
+		Scanner mainScanner = new Scanner(System.in);
+		int searchText = mainScanner.nextInt();
+		
+		while (searchText != 0) {
+			
+			if (searchText == 1) {
+				
+				for (Product p : inventory) {
+					System.out.println("\n");
+					System.out.println("Name: " + p.name);
+					System.out.println("Price: " + p.price);
+					System.out.println("Quantity: " + p.quantity);
+				} mainScanner.nextInt();
+				
+			} else if (searchText == 2) {
+				System.out.println("Unesite parametar pretrage:");
+				Scanner scanner = new Scanner(System.in);
+				String searchInventory = scanner.nextLine();
+				
+				for (Product p : inventory) {
+					if (p.name.toLowerCase().contains(searchInventory.toLowerCase())) {
+						System.out.println("\n");
+						System.out.println("Name: " + p.name);
+						System.out.println("Price: " + p.price);
+						System.out.println("Quantity: " + p.quantity);
+					}
+				}
+			} else if (searchText == 3) {
+				int sum = 0;
+				for (Product p : inventory) {
+					double price = p.price;
+					double quantity = p.quantity;
+					sum += price * quantity;
+				}
+				System.out.println("Total value is: " + sum);
+				break;
+			} else {
+				System.out.println("Not a valid Option..");
+				System.out.println("Enter your value: ");
+				mainScanner.nextInt();
+			}
 			
 		}
-		
-		System.out.println("Total value is: " + sum);
-		
-		
-//		System.out.println("Unesite parametar pretrage:");
-//		Scanner scanner = new Scanner(System.in);
-//		String searchText = scanner.nextLine();
-//		
-//		for (HashMap<String, String> p : inventory) {
-//			if (p.get("name").toLowerCase().contains(searchText.toLowerCase())) {
-//				System.out.println("\n");
-//				System.out.println("Name: " + p.get("name"));
-//				System.out.println("Price: " + p.get("price"));
-//				System.out.println("Quantity: " + p.get("quantity"));
-//			}
-//		}
-		
 
 	}
 
@@ -47,6 +67,14 @@ public class Store {
 		inventory.add(new Product("Pegla XX",(double) 1990,(double) 7));
 		inventory.add(new Product("TV samsung 3",(double) 8000,(double) 2));
 		return inventory;
+	}
+	
+	public static void pocetniMeni() {
+		System.out.println("--- Welcome to Our Store ---");
+		System.out.println("- 1) All Inventory");
+		System.out.println("- 2) Search Inventory");
+		System.out.println("- 3) Inventory value");
+		System.out.println("- 0) Exit");
 	}
 
 }
